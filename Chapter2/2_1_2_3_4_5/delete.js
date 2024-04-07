5cc80d3ac50951309191a79418d32e0110601735
janin0318 <janin-ou@outlook.jp>
2024-04-07 20:41:04 +0900

    Chapter2-4まで完了

---
+++ Chapter2/2_1_2_3_4_5/delete.js
@@ -0,0 +1,20 @@
+var data = ['JavaScript', 'Ajax', 'ASP.NET'];
+
+console.log(delete data[0]);
+console.log(data);
+
+var obj = {x: 1, y: 2};
+console.log(delete obj.x);
+console.log(obj.x);
+
+var obj2 = {x: obj, y: 2}
+console.log(delete obj2.x);
+console.log(obj2);
+
+var data1 = 1;
+console.log(delete data1);
+console.log(data1);
+
+data2 = 10;
+console.log(delete data2);
+console.log(data2);
