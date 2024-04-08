cb5b6b29b3c2326c1bb047345b55feef8ef61796
janin0318 <janin-ou@outlook.jp>
2024-04-08 23:45:27 +0900

    Chapter3 その1

---
+++ Chapter3/string.js
@@ -0,0 +1,7 @@
+var str1 = 'にわにはにわにわとりがいる';
+
+console.log(str1.indexOf('にわ'));
+console.log(str1.lastIndexOf('にわ'));
+console.log(str1.indexOf('にわ', 3));
+console.log(str1.lastIndexOf('わ', 5));
+console.log(str1.indexOf('ガーデン'));
